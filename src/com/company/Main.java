5397f3c635f7962046088adf6d51ab5fc9546f04
hrithikt <hrithikt1999@gmail.com>
2021-07-29 16:21:54 +0530

    Implemented some basic error handling.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,11 +17,10 @@ public class Main {
         while (true){
             System.out.print("Principal ($1K - $1M): ");
             principal = scan.nextLong();
-            if (!(principal >= 1_000 && principal <= 1_000_000)) {
-                System.out.println("Enter a number between 1,000 and 1,000,000.");
-                continue;
+            if (principal >= 1_000 && principal <= 1_000_000) {
+                break;
             }
-            break;
+            System.out.println("Enter a number between 1,000 and 1,000,000.");
         }
 
         //Annual interest rate
@@ -29,11 +28,10 @@ public class Main {
         while (true) {
             System.out.print("Annual Interest Rate: ");
             interestRate = scan.nextFloat();
-            if (!(interestRate > 0 && interestRate <= 30)) {
-                System.out.println("Enter a value greater than 0 and less than or equal to 30.");
-                continue;
+            if (interestRate > 0 && interestRate <= 30) {
+                break;
             }
-            break;
+            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
         }
 
         // Period
@@ -41,11 +39,10 @@ public class Main {
         while (true){
             System.out.print("Period(Years): ");
             period = scan.nextInt();
-            if (!(period >= 1 && period <= 30)) {
-                System.out.println("Enter a value between 1 and 30.");
-                continue;
+            if (period >= 1 && period <= 30) {
+                break;
             }
-            break;
+            System.out.println("Enter a value between 1 and 30.");
         }
 
         float monthlyInterestRate = interestRate/PERCENT/MONTHS_IN_A_YEAR;
